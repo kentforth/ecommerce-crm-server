9bc75c16bacf9bfa2e82f779f5164a7e9695447e
Konstantin Rusanov <rusanov@uteka.ru>
2024-10-13 19:52:32 +0700

    Add port from .env file

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"log"
+	"os"
 )
 
 func main() {
@@ -23,6 +24,6 @@ func main() {
 
 	//utils.ConnectDatabase()
 
-	app.Run(":5500")
+	app.Run(":" + os.Getenv("PORT"))
 
 }
